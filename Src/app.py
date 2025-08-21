ad2fe41f1f6147afa4fdbb9d5e3393089c59f8a4
Wajid <errormrcoder@example.com>
2025-08-21 18:28:49 +0500

    Connect DB

--- Src/app.py
+++ Src/app.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template as render
+from flask_sqlalchemy import SQLAlchemy as sql
+
 
 # Initialize Flask app
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sqlite.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = sql(app)
+
+
 
 # Home route
 @app.route("/")
